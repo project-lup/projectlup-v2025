6f21286607ce1a281792f7a35f5ffcb388f5fc4e
KimHynJun <proress@naver.com>
2025-11-05 19:24:03 +0900

    feat(FW) : add inventory

---
+++ Assets/Scripts/Data/IInventoryItem.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public interface IInventoryItem
+{
+    int ItemID { get; }
+    string ItemName { get; }
+    Sprite Icon { get; }
+
+    ItemRuntimeData ToSaveData();
+
+    void FromSaveData(ItemRuntimeData SaveData);
+}
