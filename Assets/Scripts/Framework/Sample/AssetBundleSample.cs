cd4b45f97875e643a3d4031071dc5ff1b6bc87d6
정도훈 <jdh1669@naver.com>
2025-11-06 17:33:20 +0900

    feat(FW)add sample
    에셋번들 샘플코드 추가

---
+++ Assets/Scripts/Framework/Sample/AssetBundleSample.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class AssetBundleSample : MonoBehaviour
+{
+    public RoguelikeStaticData characterdata;
+
+
+    void Start()
+    {
+        AssetBundle runtimedataAB = AssetBundle.LoadFromFile(Path.Combine(Application.dataPath, Path.Combine("Resources/AssetBundles", "staticdatas")));
+
+        if (runtimedataAB == null)
+        {
+            Debug.Log("Failed to load AssetBundle!");
+            return;
+        }
+        characterdata = runtimedataAB.LoadAsset<RoguelikeStaticData>("RoguelikeStaticData");
+
+    }
+}
