be04f2e387ef917ec29772ad48e25825233295a1
정도훈 <jdh1669@naver.com>
2025-11-06 16:45:03 +0900

    feat(FW) add function
    한번에 에셋번들들 만드는 기능 추가

--- Assets/Scripts/Framework/AutoSceneRegister.cs
+++ Assets/Scripts/Framework/AutoSceneRegister.cs
@@ -1,10 +1,13 @@
-﻿using UnityEditor;
+﻿#if UNITY_EDITOR
+using UnityEditor;
+#endif
 using UnityEngine;
 using System.Linq;
 using System.IO;
 
 public class AutoSceneRegister
 {
+#if UNITY_EDITOR
     [MenuItem("Tools/Build/Auto Register Scenes")]
     public static void AutoRegisterScenes()
     {
@@ -22,4 +25,5 @@ public class AutoSceneRegister
 
         Debug.Log($"✅ {newSceneList.Length}개의 씬이 빌드 설정에 자동 등록되었습니다!");
     }
+#endif
 }
