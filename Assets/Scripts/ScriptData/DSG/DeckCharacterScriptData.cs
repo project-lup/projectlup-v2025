2d1a459826cf8473893f30ded3ddbf3a2d9c8b92
KimHynJun <proress@naver.com>
2025-11-07 18:32:21 +0900

    feat(FW) : Add StaticSheet & Folder

---
+++ Assets/Scripts/ScriptData/DSG/DeckCharacterScriptData.cs
@@ -0,0 +1,8 @@
+﻿[System.Serializable]
+public class DeckCharacterScriptData
+{
+    [Column("CharacterId")] public int CharacterId;
+    [Column("CharacterName")] public string CharacterName;
+    [Column("AttributeType")] public int AttributeType;
+    [Column("RangeType")] public int RangeType;
+}
