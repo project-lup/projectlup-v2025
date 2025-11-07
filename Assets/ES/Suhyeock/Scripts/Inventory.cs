46331ee8e9781dad2f3beb776b8562278ffcfedc
LimSungMin <limsm620@naver.com>
2025-11-07 18:03:37 +0900

    feat(ST) : Make DataTable

--- Assets/ES/Suhyeock/Scripts/Inventory.cs
+++ Assets/ES/Suhyeock/Scripts/Inventory.cs
@@ -2,64 +2,59 @@
 using System;
 using UnityEngine;
 
-namespace ES
+public class Inventory : MonoBehaviour
 {
-    public class Inventory : MonoBehaviour
+    public event Action OnInventoryUpdated;
+    public int inventorySize = 30;
+    public List<InventorySlot> slots;
+
+    virtual public void Awake()
     {
-        public event Action OnInventoryUpdated;
-        public int inventorySize = 30;
-        public List<InventorySlot> slots;
-
-        virtual public void Awake()
+        slots = new List<InventorySlot>(inventorySize);
+        for (int i = 0; i < inventorySize; i++)
         {
-            slots = new List<InventorySlot>(inventorySize);
-            for (int i = 0; i < inventorySize; i++)
-            {
-                slots.Add(new InventorySlot());
-            }
+            slots.Add(new InventorySlot());
         }
+    }
 
-        public bool AddItem(Item item)
+    public bool AddItem(Item item)
+    {
+        // 1. 스택 가능한 아이템이라면 기존 슬롯 검색 후 추가 (Stacking Logic)
+        if(item.baseItem.type == ItemType.Consumable || item.baseItem.type == ItemType.Material)
         {
-            // 1. 스택 가능한 아이템이라면 기존 슬롯 검색 후 추가 (Stacking Logic)
-            if (item.baseItem.type == ItemType.Consumable || item.baseItem.type == ItemType.Material)
+            for (int i = 0; i < slots.Count; i++)
             {
-                for (int i = 0; i < slots.Count; i++)
+                InventorySlot slot = slots[i];
+                if (slot.item == null)
+                    continue;
+                if (slot.item.baseItem.id == item.baseItem.id)
                 {
-                    InventorySlot slot = slots[i];
-                    if (slot.item == null)
-                        continue;
-                    if (slot.item.baseItem.id == item.baseItem.id)
+                    int canAdd = slot.item.baseItem.stackSize - slot.item.count;
+
+                    int actualAdd = Math.Min(canAdd, item.count);
+                    
+                    slot.item.count += actualAdd;
+                    item.count -= actualAdd;
+
+                    if (item.count <= 0)
                     {
-                        int canAdd = slot.item.baseItem.stackSize - slot.item.count;
-
-                        int actualAdd = Math.Min(canAdd, item.count);
-
-                        slot.item.count += actualAdd;
-                        item.count -= actualAdd;
-
-                        if (item.count <= 0)
-                        {
-                            OnInventoryUpdated?.Invoke();
-                            return true;
-                        }
+                        OnInventoryUpdated?.Invoke();
+                        return true;
                     }
                 }
             }
-
-            // 2. 새로운 슬롯에 아이템 추가
-            InventorySlot emptySlot = slots.Find(s => s.IsEmpty);
-            if (emptySlot != null)
-            {
-                emptySlot.item = item;
-
-                OnInventoryUpdated?.Invoke();
-                return true;
-            }
-
-            return false; // 인벤토리가 가득 참
         }
+
+        // 2. 새로운 슬롯에 아이템 추가
+        InventorySlot emptySlot = slots.Find(s => s.IsEmpty);
+        if (emptySlot != null)
+        {
+            emptySlot.item = item;
+
+            OnInventoryUpdated?.Invoke();
+            return true;
+        }
+
+        return false; // 인벤토리가 가득 참
     }
 }
-
-
---
+++ Assets/Scripts/ScriptData/ST/ShootingScriptData.cs
@@ -0,0 +1,21 @@
+﻿[System.Serializable]
+public class ShootingScriptData
+{
+    [Column("Id")] public int Id;
+    [Column("Type")] public string Type;
+    [Column("Hp")] public int Hp;
+    [Column("Atk")] public int Atk;
+    [Column("Def")] public int Def;
+    [Column("Spd")] public float Spd;
+    [Column("AtkRange")] public float AtkRange;
+    [Column("AtkSpeed")] public float AtkSpeed;
+    [Column("AtkCooldown")] public float AtkCooldown;
+    [Column("BulletSpeed")] public float BulletSpeed;
+    [Column("SpecialAbility")] public string SpecialAbility;
+    [Column("SkillType")] public string SkillType;
+    [Column("SkillTarget")] public string SkillTarget;
+    [Column("SkillEffect")] public string SkillEffect;
+    [Column("SkillFeature")] public float SkillFeature; //퍼센테이지로 증감 되는거 까먹지말기
+    [Column("Name")] public string Name;
+    [Column("Description")] public string Description;
+}
